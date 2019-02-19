80ca1b1a0263c254c9342f03ac9e363fc76a3118
Ally-R <ally.rogers.mail@gmail.com>
2019-02-18 18:25:45 -0600

    Working on Population and Organism classes.

--- src/organismModeling/Organism.java
+++ src/organismModeling/Organism.java
@@ -1,5 +1,55 @@
 package organismModeling;
 
 public class Organism {
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+  int energy;
 
-}
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+  public Organism() {
+    this.energy = 0;
+  } // Organism()
+
+  // +---------+-----------------------------------------------------------
+  // | Methods |
+  // +---------+
+  public void update() {
+    this.incrementEnergy();
+  } // update()
+
+  public int getEnergy() {
+    return this.energy;
+  } // getEnergy()
+
+  public void incrementEnergy() {
+    this.energy += 1;
+  } // incrementEnergy()
+
+  public void decrementEnergy() {
+    if (this.energy > 0) {
+      this.energy -= 1;
+    } // if
+  } // decrementEnergy()
+
+  public String getType() { // To be overwritten
+    return "Organism";
+  } // getType()
+
+  public Organism reproduce() { // To be overwritten
+    Organism offspring = new Organism();
+    offspring = this;
+    return offspring;
+  } // reproduce()
+  
+  public double getCooperationProbability() { // To be overwritten
+    return 0;
+  } // getCooperationProbability()
+  
+  public boolean cooperates() { // To be overwritten
+    return false;
+  } // cooperates()
+
+} // class Organism
--- src/organismModeling/Population.java
+++ src/organismModeling/Population.java
@@ -4,18 +4,48 @@ public class Population {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
-  // List of organisms
-  
+  Organism[] population;
+
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
   public Population(Pair<String, Integer>[] counts) throws IllegalArgumentException {
-    
+    int popsize = 0;
+    for (int i = 0; i < counts.length; i++) {
+      popsize += counts[i].getRight();
+    } // for
+    this.population = new Organism[popsize];
+    int place = 0; // Current position in population
+    for (int i = 0; i < counts.length; i++) { // Iterates through counts
+      String currentType = counts[i].getLeft();
+      for (int k = place; k < (place + counts[i].getRight()); k++) { // Iterates through population,
+                                                                     // adding organisms
+        if (currentType.equals("Cooperator")) {
+          population[k] = new Cooperator();
+        } else if (currentType.equals("Defector")) {
+          population[k] = new Defector();
+        } else if (currentType.equals("PartialCooperator")) {
+          population[k] = new PartialCooperator();
+        } else {
+          throw new IllegalArgumentException(
+              "Input " + currentType + " invalid. Must be either Cooperator, Defector, or PartialCooperator.");
+        } // else
+      } // for
+    } // for
   } // Population(Pair<String, Integer>[] counts)
-  
+
   // +---------+-----------------------------------------------------------
   // | Methods |
   // +---------+
-  
-  
+
 } // class Population
+
+/*
+   public void checkType(String type) throws IllegalArgumentException {
+    if (!(type.equals("Cooperator") || type.equals("Defector")
+        || type.equals("PartialCooperator"))) {
+      throw new IllegalArgumentException(
+          "Input " + type + " invalid. Must be either Cooperator, Defector, or PartialCooperator.");
+    } // if
+  } // checkType(String type)
+*/
