74fcfeaa1913693f318e444c465e7c6c8fdb04e2
Ally-R <ally.rogers.mail@gmail.com>
2019-02-19 20:45:55 -0600

    more stuff on Population class

--- src/organismModeling/Population.java
+++ src/organismModeling/Population.java
@@ -63,5 +63,37 @@ public class Population {
       } // if energy >= 10
     } // for
   } // update()
+  
+  public double calculateCooperationMean() {
+    int popSize = this.population.length;
+    double totalProb = 0;
+    for (int i = 0; i < popSize; i++) {
+      totalProb += this.population[i].getCooperationProbability();
+    } // for
+    return totalProb/popSize;
+  } // calculateCooperationMean()
+  
+  public Pair<String, Integer>[] getPopulationCounts() {
+    int popSize = this.population.length;
+    Pair<String,Integer>[] popCount = (Pair<String,Integer>[]) (new Pair[3]);
+    popCount[0] = new Pair<String,Integer>("Collaborator", 0);
+    popCount[1] = new Pair<String,Integer>("Defector", 0);
+    popCount[2] = new Pair<String,Integer>("PartialCooperator", 0);
+    String currentType;
+    for (int i = 0; i < popSize; i++) {
+      currentType = this.population[i].getType();
+      if (currentType.equals("Cooperator")) {
+        popCount[0].right += 1;
+      } else if (currentType.equals("Defector")) {
+        popCount[1].right += 1;
+      } else if (currentType.equals("PartialCooperator")) {
+        popCount[2].right += 1;
+      } else {
+        throw new IllegalArgumentException(
+            "The " + currentType + "organism is invalid. Must be either Cooperator, Defector, or PartialCooperator.");
+      } // else
+    } // for
+    return popCount;
+  } // getPopulationCounts()
 
 } // class Population
